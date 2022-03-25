e4522895426746edcfc83f5d96327b99fb46402c
RunningTHPed <pednoiiz008@gmail.com>
2022-03-25 23:35:54 +0700

    structure solidity

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract MyContract{
+    
+    //define variable
 
+    /*
+    structure definition variable
+    type access_modifier name;
+    */
+
+    bool status = false;
+    string name = "TestUser";
+    int amount = 5000;
+    uint balance = 1000;
 }
